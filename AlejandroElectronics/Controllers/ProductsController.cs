7850d40fb47f03c519371039f48b43860ad30a6c
abiancu <alejandrobiancucci@gmail.com>
2018-02-13 16:18:45 -0600

    Query
    typing query = to... in the brwoser, displays the images and the right product.

--- AlejandroElectronics/Controllers/ProductsController.cs
+++ AlejandroElectronics/Controllers/ProductsController.cs
@@ -8,20 +8,40 @@ namespace AlejandroElectronics.Controllers
 {
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+    
+
+        public IActionResult Index(int Sku)
         {
             Models.ProductsViewModel model = new Models.ProductsViewModel();
-            model.Name = "Albert";
-            model.Price = 299.99m;
-            model.DietaryRequirements = "Grass, Hay, Carrots, Cap'n Crunch";
-            model.Description = "Albert is an absolutely perfect Alpaca";
-            model.Age = 4;
-            model.Temperment = "Pleasant";
-            model.ImageUrl = "/images/computer1.jpg";
+            if(Sku==300)
+            {
+                          
+                model.Name = "Mac Air";
+                model.Price = 299.99m;
+                model.Sku = 300;
+                model.Description = "Pretty cool laptop";
+                model.ImageUrl = "/images/computer1.jpg";
+            } 
+            else if (Sku==400)
+            {
+                model.Name = "Surface";
+                model.Price = 399.99m;
+                model.Sku = 400;
+                model.Description = "Pretty cool laptop";
+
+            }
+            else if (Sku == 500)
+            {
+                model.Name = "Toshiba";
+                model.Price = 899.99m;
+                model.Sku = 500;
+                model.Description = "It's Ok!";
+                model.ImageUrl = "/images/toshiba.jpg";
+            }
+            
            
-
-
             return View(model);
+
         }
     }
 }
--- AlejandroElectronics/Models/ProductsViewModel.cs
+++ AlejandroElectronics/Models/ProductsViewModel.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace AlejandroElectronics.Models
 {
+    
     public class ProductsViewModel
     {
+        
         public string Name { get; set; }
-        public int Age { get; set; }
-        public string Description { get; set; }
-        public string DietaryRequirements { get; set; }
-        public string Temperment { get; set; }
+        public int Sku { get; set; }
+        public string Description { get; set; }        
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
 
