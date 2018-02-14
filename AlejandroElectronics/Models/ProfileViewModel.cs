67cbfaa6faeb99734c980343e211fe5846c56b16
abiancu <alejandrobiancucci@gmail.com>
2018-02-14 11:29:18 -0600

    Created Profile page

---
+++ AlejandroElectronics/Models/ProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+
+
+
+namespace AlejandroElectronics.Models
+{
+    public class ProfileViewModel
+    {
+       
+       public string name { get; set; }
+       public string password { get; set; }
+       public string username { get; set; }
+       public string address { get; set; }
+       public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+
+
+    }
+}
