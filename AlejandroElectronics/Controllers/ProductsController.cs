02778bb933b5efd493c07dfdff51ac75338aa559
abiancu <alejandrobiancucci@gmail.com>
2018-02-13 20:32:09 -0600

    products into array
    converted products into array so it displays all products in the product route

--- AlejandroElectronics/Controllers/ProductsController.cs
+++ AlejandroElectronics/Controllers/ProductsController.cs
@@ -8,35 +8,41 @@ namespace AlejandroElectronics.Controllers
 {
     public class ProductsController : Controller
     {
-    
+
 
         public IActionResult Index(int Sku)
         {
             Models.ProductsViewModel model = new Models.ProductsViewModel();
-            if(Sku==300)
+            model.Products = new Models.Product[]
             {
-                          
-                model.Name = "Mac Air";
-                model.Price = 299.99m;
-                model.Sku = 300;
-                model.Description = "Pretty cool laptop";
-                model.ImageUrl = "/images/computer1.jpg";
-            } 
-            else if (Sku==400)
-            {
-                model.Name = "Surface";
-                model.Price = 399.99m;
-                model.Sku = 400;
-                model.Description = "Pretty cool laptop";
+                new Models.Product
+                {
+                    Name = "Mac Air",
+                    Price = 299.99m,
+                    Sku = 300,
+                        Description = "Pretty cool laptop",
+                        ImageUrl = "/images/computer1.jpg"
+                },
+                new Models.Product
+                {
+                     Name = "Surface",
+                    Price = 399.99m,
+                    Sku = 400,Description = "Pretty cool laptop",
+                    ImageUrl = "/images/surface.jpg"
 
-            }
-            else if (Sku == 500)
+                },
+                new Models.Product{
+
+                    Name = "Toshiba",
+                    Price = 899.99m,
+                    Sku = 500,
+                    Description = "It's Ok!",
+                    ImageUrl = "/images/toshiba.jpg"
+                }
+            };
+            if(Sku > 0)
             {
-                model.Name = "Toshiba";
-                model.Price = 899.99m;
-                model.Sku = 500;
-                model.Description = "It's Ok!";
-                model.ImageUrl = "/images/toshiba.jpg";
+                model.Products = model.Products.Where(x => x.Sku == Sku).ToArray();
             }
             
            
--- AlejandroElectronics/Models/ProductsViewModel.cs
+++ AlejandroElectronics/Models/ProductsViewModel.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 
 namespace AlejandroElectronics.Models
 {
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Sku { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
     
     public class ProductsViewModel
     {
-        
-        public string Name { get; set; }
-        public int Sku { get; set; }
-        public string Description { get; set; }        
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
-
+       public Product[] Products { get; set; } 
     }
 }
